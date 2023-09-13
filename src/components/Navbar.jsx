72fbd65ffc195d822058c19d7769f6fe32a354aa
Pawel Jaworski Software <jpsoftware@MacBook-Pro-Pawel.local>
2023-09-14 01:37:43 +0200

    components search & navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
 
 const Navbar = () => {
-    return ( <div>Navbar</div> );
+    return (
+         <div className='navbar flex flex-direction:row justify-between items-center p-[3rem]'>
+             <div className='logoDiv'>
+                <h1 className='logo text-[45px] text-blueColor'>
+                    <strong>Job</strong>Search
+                </h1>
+             </div>
+             <div className='menu m-2 flex gap-4'>
+                <li className='menuList text-[#6f6f6f] hover:text-blueColor'>Jobs</li>
+                <li className='menuList text-[#6f6f6f] hover:text-blueColor'>Companies</li>
+                <li className='menuList text-[#6f6f6f] hover:text-blueColor'>About</li>
+                <li className='menuList text-[#6f6f6f] hover:text-blueColor'>Contact</li>
+                <li className='menuList text-[#6f6f6f] hover:text-blueColor'>Blog</li>
+                <li className='menuList text-[#6f6f6f] hover:text-blueColor'>Login</li>
+                <li className='menuList text-[#6f6f6f] hover:text-blueColor'>Register</li>
+             </div>
+        </div>
+    );
 }
  
 export default Navbar;
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,38 @@
 import React from 'react'
 
+import {AiOutlineCloseCircle, AiOutlineSearch} from 'react-icons/ai'
+import { BsHouseDoor } from 'react-icons/bs'
+import { CiLocationOn } from 'react-icons/ci'
+
 const Search = () => {
-    return ( <div>Search</div> );
+    return (
+         <div className='searchDiv grid gp-10 bg-greyIsh rounded-[10px] p-[3rem]'>
+             <form action="">
+                <div className='firstDiv flex justify-between items-center rounded-[8px] gap-[10px] bg-white p-5 shadow-lg shadow-greyIsh-700'>
+                     <div className='flex gap-2 items-center'>
+                         <AiOutlineSearch className='text-[25px] icon'/>
+                         <input type="text" className='bg-transparent text-blue-500 focus:outline-none w-[100%]' placeholder='Search Job Here...'/>
+                         <AiOutlineCloseCircle className='text-[30px] text-[#a5a6a6] hover:text-textColor icon'/>
+                    </div>
+
+                    <div className='flex gap-2 items-center'>
+                         <BsHouseDoor className='text-[25px] icon'/>
+                         <input type="text" className='bg-transparent text-blue-500 focus:outline-none w-[100%]' placeholder='Search by company...'/>
+                         <AiOutlineCloseCircle className='text-[30px] text-[#a5a6a6] hover:text-textColor icon'/>
+                    </div>
+
+                    <div className='flex gap-2 items-center'>
+                         <CiLocationOn className='text-[25px] icon'/>
+                         <input type="text" className='bg-transparent text-blue-500 focus:outline-none w-[100%]' placeholder='Search by location...'/>
+                         <AiOutlineCloseCircle className='text-[30px] text-[#a5a6a6] hover:text-textColor icon'/>
+                    </div>
+
+                    <button className='bg-blueColor h-full p-5 px-10 rounded-[10px] text-white cursor-pointer hover:bg-blue-300'>Search</button>
+
+                </div>
+             </form>
+         </div>
+    );
 }
  
 export default Search;
