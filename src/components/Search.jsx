e4deb9687cfb62e96aabed390b478c76638cefe1
Pawel Jaworski Software <jpsoftware@MacBook-Pro-Pawel.local>
2023-09-16 19:16:51 +0200

    search_sortMenu

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-import {AiOutlineCloseCircle, AiOutlineSearch} from 'react-icons/ai'
+import { AiOutlineCloseCircle, AiOutlineSearch} from 'react-icons/ai'
 import { BsHouseDoor } from 'react-icons/bs'
 import { CiLocationOn } from 'react-icons/ci'
 
 const Search = () => {
     return (
-         <div className='searchDiv grid gp-10 bg-greyIsh rounded-[10px] p-[3rem]'>
+         <div className='searchDiv grid gap-10 bg-greyIsh rounded-[10px] p-[3rem]'>
              <form action="">
                 <div className='firstDiv flex justify-between items-center rounded-[8px] gap-[10px] bg-white p-5 shadow-lg shadow-greyIsh-700'>
                      <div className='flex gap-2 items-center'>
@@ -31,6 +31,41 @@ const Search = () => {
 
                 </div>
              </form>
+            <div className='secDiv flex items-center gap-10 justify-center'>
+
+                <div className='singleSearch flex items-center gap-2'>
+                    <label htmlFor='relevance' className='text-[#808080] font-semibold'>Sort by:</label>
+                    <select name="" id="relevance" className='bg-white rounded-[3px] px=4 py-1'>
+                        <option value="">Relevance</option>
+                        <option value="">Inclusive</option>
+                        <option value="">Starts With</option>
+                        <option value="">Contains</option>
+                    </select>
+                </div>
+
+                <div className='singleSearch flex items-center gap-2'>
+                    <label htmlFor='type' className='text-[#808080] font-semibold'>Type:</label>
+                    <select name="" id="type" className='bg-white rounded-[3px] px=4 py-1'>
+                        <option value="">Contract</option>
+                        <option value="">Full-Time</option>
+                        <option value="">Part-Time</option>
+                        <option value="">Remote</option>
+                    </select>
+                </div>
+
+                <div className='singleSearch flex items-center gap-2'>
+                    <label htmlFor='level' className='text-[#808080] font-semibold'>Level:</label>
+                    <select name="" id="level" className='bg-white rounded-[3px] px=4 py-1'>
+                        <option value="">Senior</option>
+                        <option value="">Middle</option>
+                        <option value="">Junior</option>
+                        <option value="">Intern</option>
+                    </select>
+                </div>
+
+                <span className='text-[#a1a1a1] cursor-pointer'>Clear All</span>
+
+            </div>
          </div>
     );
 }
