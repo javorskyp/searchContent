bd16e51d713bef13f4821243bda524779de88397
Pawel Jaworski Software <jpsoftware@MacBook-Pro-Pawel.local>
2023-09-28 21:32:16 +0200

    value_content

--- src/components/Value.jsx
+++ src/components/Value.jsx
@@ -9,10 +9,17 @@ const Value = () => {
             <div className='grid gap-[10rem] grid-cols-3 items-center'>
                 <div className='singleGrid rounded-[10px] hover:bg-[#eeef7] p-[1.5rem]'>
                     <div className='flex items-center gap-3'>
-                        <div className='imgDiv p-[4px] rounded-[.8rem] bg-inherit-[#dedef8] h-[40px] w-[40px] flex items-center justify-center'>
-                            <img src="" alt />
+                        <div className='imgDiv p-[4px] rounded-[.8rem] bg-[#dedef8] h-[40px] w-[40px] flex items-center justify-center'>
+                            <img src="" alt className='w-[70%]'/>
                         </div>
+
+                        <span className='font-semibold text-textColor text-[18px]'>
+                            Simplicity
+                        </span>
                     </div>
+                    <p className='text-[13px] text-textColor opacity-[.7] py-[1rem] font-semibold'>
+                        Things beinf made beautiful simple are at the heart of everything we do.
+                    </p>
                 </div>
             </div>
         </div>
