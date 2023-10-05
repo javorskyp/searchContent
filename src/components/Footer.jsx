19e2277dbc7616c679698e2fdf5db60f31944604
Pawel Jaworski Software <jpsoftware@MacBook-Pro-Pawel.local>
2023-10-05 23:41:20 +0200

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import {AiFillInstagram, AiOutlineTwitter} from 'react-icons/ai'
+import { BsFacebook } from 'react-icons/bs';
 
 const Footer = () => {
     return (
@@ -8,6 +10,63 @@ const Footer = () => {
                 <h1 className='logo text-[25px] text-white pb-[1.5rem]'>
                     <strong>Job</strong>Search
                 </h1>
+            </div>
+            <p className='text-white pb-[13px] opacity-70 leading-7'>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eiussss porro doloribus sint maxime soluta quas.
+            </p>
+        </div>
+        <div className='grid'>
+            <span className='title text-[18px] font-semibold pb-[1.5rem] text-white'>
+            Company
+            </span>
+            <div className='grid gap-3'>
+
+            <li className='text-white opacity-[.7] hover:opacity-[1]'>About Us</li>
+            <li className='text-white opacity-[.7] hover:opacity-[1]'>Features</li>
+            <li className='text-white opacity-[.7] hover:opacity-[1]'>News</li>
+            <li className='text-white opacity-[.7] hover:opacity-[1]'>FAQ</li>
+
+            </div>
+        </div>
+        <div className='grid'>
+            <span className='title text-[18px] font-semibold pb-[1.5rem] text-white'>
+            Resources
+            </span>
+            <div className='grid gap-3'>
+
+            <li className='text-white opacity-[.7] hover:opacity-[1]'>Account</li>
+            <li className='text-white opacity-[.7] hover:opacity-[1]'>Support</li>
+            <li className='text-white opacity-[.7] hover:opacity-[1]'>Feedback</li>
+            <li className='text-white opacity-[.7] hover:opacity-[1]'>Contact Us</li>
+
+            </div>
+        </div>
+        <div className='grid'>
+            <span className='title text-[18px] font-semibold pb-[1.5rem] text-white'>
+            Support
+            </span>
+            <div className='grid gap-3'>
+
+            <li className='text-white opacity-[.7] hover:opacity-[1]'>Events</li>
+            <li className='text-white opacity-[.7] hover:opacity-[1]'>Promo</li>
+            <li className='text-white opacity-[.7] hover:opacity-[1]'>Docs</li>
+            <li className='text-white opacity-[.7] hover:opacity-[1]'>Careers</li>
+
+            </div>
+        </div>
+        <div className='grid'>
+            <span className='title text-[18px] font-semibold pb-[1.5rem] text-white'>
+            Contact Info
+            </span>
+            <div>
+            <small className='text-[14px] text-white'>
+                israelAdresanya@DanaWhite.com
+            </small>
+            <div className='icons flex gap-4 py-[1rem]'>
+                <AiFillInstagram classname='bg-white p-[8px] h-[35px] w-[35px] rounded-full icon text-blueColor'/>
+                <BsFacebook classname='bg-white p-[8px] h-[35px] w-[35px] rounded-full icon text-blueColor'/>
+                <AiOutlineTwitter classname='bg-white p-[8px] h-[35px] w-[35px] rounded-full icon text-blueColor'/>
+            </div>
             </div>
         </div>
     </div> 
